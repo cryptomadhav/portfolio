5f3f37493dbcb58aa86cc31f9d761dc99a271ced
cryptomadhav <cryptomadhav@gmail.com>
2020-11-12 02:34:43 +0530

    Updates

---
+++ precache-manifest.e57230975ee77c380801bef54a03e8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c2cc78fdd6ceae01e7b46bb0ca2cdc",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "92e676add6a2ac20f511",
+    "url": "/portfolio-2020/static/css/main.fa097a27.chunk.css"
+  },
+  {
+    "revision": "2e47ec679a184e3a9814",
+    "url": "/portfolio-2020/static/js/2.29f27461.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-2020/static/js/2.29f27461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e676add6a2ac20f511",
+    "url": "/portfolio-2020/static/js/main.0acdfd97.chunk.js"
+  },
+  {
+    "revision": "bbf3efb9002603ffad2a",
+    "url": "/portfolio-2020/static/js/runtime-main.106eb575.js"
+  },
+  {
+    "revision": "531344742989ff7301819164cbb5fce1",
+    "url": "/portfolio-2020/static/media/SpaceGrotesk-Regular.53134474.otf"
+  }
+]);
