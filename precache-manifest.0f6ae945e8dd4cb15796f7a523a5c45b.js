4f3271c246ad1233140e0553d9de0dfdf478deee
cryptomadhav <cryptomadhav@gmail.com>
2021-12-17 22:10:24 +0530

    Updates

---
+++ precache-manifest.0f6ae945e8dd4cb15796f7a523a5c45b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df08d7544def6249ccb21a979dcd32d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c350465d89134017dd35",
+    "url": "/portfolio/static/css/main.541d0799.chunk.css"
+  },
+  {
+    "revision": "f6b8598e5da64c83f6f9",
+    "url": "/portfolio/static/js/2.4814ba0d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4814ba0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c350465d89134017dd35",
+    "url": "/portfolio/static/js/main.753c730c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "531344742989ff7301819164cbb5fce1",
+    "url": "/portfolio/static/media/SpaceGrotesk-Regular.53134474.otf"
+  },
+  {
+    "revision": "0ad6226406fb31d8eb75322d6f6fe920",
+    "url": "/portfolio/static/media/shuttle.0ad62264.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-2020/precache-manifest.5fb1b5788122aefcd0506c2dddc6b93c.js"
+  "/portfolio/precache-manifest.0f6ae945e8dd4cb15796f7a523a5c45b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-2020/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
